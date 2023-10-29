2e577d72720593d113b45ffd889d84f3ef9c8c1d
Kishan <KishanYerneni@gmail.com>
2023-10-29 18:19:05 -0500

    Added getAvatar and reactor Slash Commands

---
+++ src/commands/tools/reactor.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('reactor')
+        .setDescription('Returns reactions'),
+
+    async execute(interaction, client) {
+        const message = await interaction.reply({
+            content: 'React Here!',
+            fetchReply: true,
+        });
+
+        const emoji = client.emojis.cache.find(
+            (emoji) => emoji.id == '1168322296210002025'
+        );
+
+        message.react(emoji);
+
+        const filter = (reaction, user) => {
+            return (
+                reaction.emoji.id == emoji.id && interaction.user.id == user.id
+            );
+        };
+
+        const collector = message.createReactionCollector({
+            filter,
+            time: 5000,
+        });
+
+        const reactedUsers = [];
+
+        collector.on('collect', (reaction, user) => {
+            console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
+            reactedUsers.push(user.tag);
+        });
+
+        collector.on('end', (collected) => {
+            console.log(`Collected ${collected.size} items`);
+        });
+    },
+};
--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -15,9 +15,6 @@ module.exports = (client) => {
                 const command = require(`../../commands/${folder}/${file}`);
                 commands.set(command.data.name, command);
                 commandArray.push(command.data.toJSON());
-                console.log(
-                    `Command: ${command.data.name} has passed through the handler`
-                );
             }
         }
 
