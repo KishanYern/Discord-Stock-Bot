b2551d693fdf51b390a7519df4f89fc7a89748f3
Kishan <KishanYerneni@gmail.com>
2023-11-05 00:30:10 -0500

    modified register command

--- src/commands/tools/Register.js
+++ src/commands/tools/Register.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('Register')
+        .setName('register')
         .setDescription('Registers user')
         .addStringOption((option) =>
             option
