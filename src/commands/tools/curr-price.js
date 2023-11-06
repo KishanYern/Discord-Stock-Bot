37b03de92f739cdf721b4d8d4d88342829b52c55
Kishan <KishanYerneni@gmail.com>
2023-11-06 14:40:49 -0600

    test push

--- src/commands/tools/curr-price.js
+++ src/commands/tools/curr-price.js
@@ -16,7 +16,7 @@ module.exports = {
         const name = interaction.options.getString('stock-name');
         const { stocks } = client;
         try {
-            // fix from here
+            // fix from here hello
             const results = await liveStockPrice(name);
             await interaction.reply({
                 content: `The current stock price of **${name}** is: $${price}`,
